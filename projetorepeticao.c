ed75f2d4768ffb2c6dec087fc42daeb3bf5a6127
jhonsalves <107511083+jhonsalves@users.noreply.github.com>
2022-06-14 17:28:19 -0300

    Add files via upload

---
+++ projetorepeticao.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int cont = 0;
+	
+	for (int valor = 7; valor <=13; valor++)
+	{
+		cont = cont + 1;
+	}
+	
+	
+	return 0;
+}
